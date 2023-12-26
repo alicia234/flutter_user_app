10bc57c82175e5c8bfbe7bbe78f1251dd511cd6d
alicia234 <201240001054@gmail.com>
2023-12-26 21:28:08 +0700

    meet 11

--- lib/common/global_variables.dart
+++ lib/common/global_variables.dart
@@ -1,3 +1,3 @@
 class GlobalVariables {
-  static const baseUrl = 'https://34e9-2404-8000-1028-646-341a-3c2a-2aef-b60b.ngrok-free.app';
+  static const baseUrl = 'https://f7e7-2404-8000-1028-646-341a-3c2a-2aef-b60b.ngrok-free.app';
 }
